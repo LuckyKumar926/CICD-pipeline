37fdacc670eb622f1b749bcfff28016792d9e45f
LuckyKumar926 <kumarlaxmangc@gmail.com>
2024-05-24 11:37:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end("Hello world tssssssfrrhis is new ")
+    res.end("Hello world change in the code deploy ")
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
